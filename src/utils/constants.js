fb37a4371bc8948f3b698e9addd4d6ab0ec2a5c3
Shivam Kumar <life.shivam2394@gmail.com>
2024-06-24 00:57:27 +0530

    structure done

---
+++ src/utils/constants.js
@@ -0,0 +1,39 @@
+export const questionBank = [
+    {
+        id: 1,
+        label: 'I have ambitious aims of making a difference.',
+        answer: '',
+        value: undefined,
+    },
+    {
+        id: 2,
+        label: 'My leadership journey has progressed as I anticipated.',
+        answer: '',
+        value: undefined,
+    },
+    {
+        id: 3,
+        label: 'I have spent fewer than 4 years in full time service or ministry.',
+        answer: '',
+        value: undefined,
+    },
+    {
+        id: 4,
+        label:
+            'With hard work and determination, I have been able to persevere through the ministry challenges that have come my way.',
+        answer: '',
+        value: undefined,
+    },
+    {
+        id: 5,
+        label: 'My plans are likely to succeed.',
+        answer: '',
+        value: undefined,
+    },
+    {
+        id: 6,
+        label: 'I have ambitious aims of making a difference.',
+        answer: '',
+        value: undefined,
+    },
+];
